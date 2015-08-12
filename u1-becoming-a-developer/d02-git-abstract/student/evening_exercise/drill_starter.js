aad0325a8482e489b20e6f4883a462f2c5cd2045
Jennifer Yien <jenniferllwu@gmail.com>
2015-08-11 20:13:31 -0400

    finished Part 2 Basic Conversion - Pounds to kg

--- u1-becoming-a-developer/d02-git-abstract/student/evening_exercise/drill_starter.js
+++ u1-becoming-a-developer/d02-git-abstract/student/evening_exercise/drill_starter.js
@@ -2,3 +2,8 @@
 var side1 = prompt("What is the length of the first side?");
 var side2 = prompt("What is the length of the second side?");
 alert("The length of the triangle with sides of length " + side1 + " and " + side2 + " is " + Math.sqrt(Math.pow(side1,2) + Math.pow(side2,2)) + ".");
+
+//Part 2 Basic Conversion - Pounds to kg
+var lbtokg = prompt("Give a weight in pounds:");
+var conversion = lbtokg * 0.453592;
+alert(lbtokg + " pounds is equal to " + conversion + " kg.");
