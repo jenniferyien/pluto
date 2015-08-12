e86e6423bbcd92a02b8fec95e3b23f57d64b8319
Jennifer Yien <jenniferllwu@gmail.com>
2015-08-11 20:33:46 -0400

    Part 3 Killer Caffeine

--- u1-becoming-a-developer/d02-git-abstract/student/evening_exercise/drill_starter.js
+++ u1-becoming-a-developer/d02-git-abstract/student/evening_exercise/drill_starter.js
@@ -7,3 +7,13 @@ alert("The length of the triangle with sides of length " + side1 + " and " + sid
 var lbtokg = prompt("Give a weight in pounds:");
 var conversion = lbtokg * 0.453592;
 alert(lbtokg + " pounds is equal to " + conversion + " kg.");
+
+//Part 3 Killer Caffeine
+var personWeight = prompt("How much do you weigh?");
+var coffeecup = personWeight * .63;
+//8oz cup = 1 cup of coffee = 95mg
+// 6 g = 6000mg
+// 6000mg/ 95mg per cup = 63.15 cups of coffee rounding to 63 cups of coffee
+//therefore 63 cups of coffee will kill a person that is 100 pounds
+// 1lb = .63 cups of coffee
+alert("It would take " + coffeecup + " cups of coffee to kill YOU.");
