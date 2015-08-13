0bc78ae7e1f587aee0fcda62712a38c252e55efd
Jennifer Yien <jenniferllwu@gmail.com>
2015-08-13 18:44:13 -0400

    Part 3 – Set up data structures

---
+++ u1-becoming-a-developer/d04-funky-loops/homework/mta.js
@@ -0,0 +1,3 @@
+var nLineStops   = ['Times Square', '34th', '28th-n', '23rd-n', 'Union Square', '8th']
+var lLineStops   = ['8th Ave', '6th Ave', 'Union Square', '3rd Ave', '1st Ave']
+var sixLineStops = ['Grand Central', '33rd', '28th-six', '23rd-six', 'Union Square', 'Astor Place']
