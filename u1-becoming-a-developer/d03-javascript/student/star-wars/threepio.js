06c14fba4a53c12c94646dc28c88e3e6494c504c
Jennifer Yien <jenniferllwu@gmail.com>
2015-08-12 21:42:20 -0400

    bonus part 1

--- u1-becoming-a-developer/d03-javascript/student/star-wars/threepio.js
+++ u1-becoming-a-developer/d03-javascript/student/star-wars/threepio.js
@@ -1,18 +1,22 @@
-window.alert("Hello, I am C-3PO, human-cyborg relations.")
+window.alert("Hello, I am C-3PO, human-cyborg relations.");
 
-var input = prompt("And your name is?");
+while(true) {
+  var input = prompt("And your name is?");
 
-if (input.toLowerCase() == "obi-wan kenobi"){
-  window.alert("Oh, marvelous! Simply marvelous! Say hello to R2-D2; he's been looking all over for you!");
-} else {
-  var question = prompt("It is a pleasure to meet you, " + input + ". I'm terribly sorry for prying, but you don't by any chance go by the alias of Obi-Wan Kenobi, do you?");
-  if (question.toLowerCase() == "yes" || question.toLowerCase() == "y"){
-    window.alert("Oh, marvelous! Simply marvelous! Say hello to R2-D2; he's been looking all over for you!");
-  } else if (question.toLowerCase() == "no" || question.toLowerCase() == "n") {
-    window.alert("I've really enjoyed speaking with you, " + input + ", but if you'll please excuse me, I have to help my friend find someone named Obi-Wan Kenobi.");
-    window.alert("Well R2, I suppose we'll just have to keep looking. R2-D2: (Agreeable droid noises)");
-  } else {
-    window.alert("I'm sorry, I didn't hear you correctly. I only respond to 'yes' or 'no'...");
-    window.alert("Well R2, I suppose we'll just have to keep looking. R2-D2: (Agreeable droid noises)");
+    if (input.toLowerCase() == "obi-wan kenobi"){
+      window.alert("Oh, marvelous! Simply marvelous! Say hello to R2-D2; he's been looking all over for you!");
+      break;
+    } else {
+      var question = prompt("It is a pleasure to meet you, " + input + ". I'm terribly sorry for prying, but you don't by any chance go by the alias of Obi-Wan Kenobi, do you?");
+      if (question.toLowerCase() == "yes" || question.toLowerCase() == "y"){
+        window.alert("Oh, marvelous! Simply marvelous! Say hello to R2-D2; he's been looking all over for you!");
+        break;
+      } else if (question.toLowerCase() == "no" || question.toLowerCase() == "n") {
+        window.alert("I've really enjoyed speaking with you, " + input + ", but if you'll please excuse me, I have to help my friend find someone named Obi-Wan Kenobi.");
+        window.alert("Well R2, I suppose we'll just have to keep looking. R2-D2: (Agreeable droid noises)");
+      } else {
+        window.alert("I'm sorry, I didn't hear you correctly. I only respond to 'yes' or 'no'...");
+        window.alert("Well R2, I suppose we'll just have to keep looking. R2-D2: (Agreeable droid noises)");
+      }
+    }
   }
-}
