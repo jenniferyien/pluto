c35621ed6d4c7c7a261ce8af228cddc3282bcf84
Jennifer Yien <jenniferllwu@gmail.com>
2015-08-14 17:38:45 -0400

    Round 3

--- u1-becoming-a-developer/d05-funky-lab/homework/reps.js
+++ u1-becoming-a-developer/d05-funky-lab/homework/reps.js
@@ -32,3 +32,25 @@ transmogrifier(5, 3, 2); //225
 transmogrifier(5, 4, 3); //8000
 transmogrifier(13, 12, 5); //92389579776
 transmogrifier(42, 13, 7); //14466001271480793000
+
+//Round 3
+function toonify(accent, sentence){
+  var newSent = '';
+  if(accent === 'daffy'){
+    for(var i=0; i < sentence.length; i++){
+      if(sentence[i] === 's'){
+        newSent = sentence.split(sentence[i]).join('th');
+      }
+    }
+  } else if (accent === 'elmer'){
+    for(var x=0; x < sentence.length; x++){
+      if(sentence[x] === 'r'){
+        newSent = sentence.split(sentence[x]).join('w');
+      }
+    }
+  }
+  return newSent;
+}
+
+toonify("daffy", "so you smell like sausage");
+toonify("elmer", "where is the rabbit?");
