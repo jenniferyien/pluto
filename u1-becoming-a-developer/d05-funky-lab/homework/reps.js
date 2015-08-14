ddbf39c94ac7150b26524118291d03f013406e25
Jennifer Yien <jenniferllwu@gmail.com>
2015-08-14 17:28:04 -0400

    round 1

--- u1-becoming-a-developer/d05-funky-lab/homework/reps.js
+++ u1-becoming-a-developer/d05-funky-lab/homework/reps.js
@@ -8,3 +8,17 @@ function lengths(array) {
   return arrayWordLength;
 }
 lengths(words);
+
+//Round 1
+function fizzbuzzer(number){
+  if (number % 3 === 0 && number % 5 === 0){
+    return "FizzBuzz";
+  } else if (number % 3 === 0){
+    return "Fizz";
+  } else if (number % 5 === 0){
+    return "Buzz";
+  } else if (number % 3 !== 0 || number % 5 !== 0){
+    return "McClane";
+  }
+}
+fizzbuzzer(5);
