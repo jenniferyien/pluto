fc989d3d341b28f5c3f072987c3ea7c7f620d944
Jennifer Yien <jenniferllwu@gmail.com>
2015-08-17 11:26:21 -0400

    classwork d06

---
+++ u2-the-browser/d06-float-my-boat/student/scope/scope.js
@@ -0,0 +1,13 @@
+function instructorNames(){
+var names = ["Jaden","Pan","Dennis","Bobby"];
+for(var  i=0; i<names.length;i++){
+  debugger;
+}
+}
+instructorNames();
+// function someFunction(someParamenter) {
+//   debugger;
+//   alert("You wrote" + someParamenter);
+// }
+// debugger;
+// someFunction("Some Value");
