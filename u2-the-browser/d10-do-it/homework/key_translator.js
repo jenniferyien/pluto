e4bdc4b4e00a9ed12f4611ea370ce3e6650151d0
Jennifer Yien <jenniferllwu@gmail.com>
2015-08-24 08:44:53 -0400

    change to caps and lowercase

--- u2-the-browser/d10-do-it/homework/key_translator.js
+++ u2-the-browser/d10-do-it/homework/key_translator.js
@@ -3,19 +3,19 @@ $(function(){
     inputText.keyup(function(event){
       var inputRead = inputText.val();
 
-      $('.box').html(inputRead.replace(/Command/g,'&#8984;')
-                                .replace(/Tab/g,'&#8677;')
-                                .replace(/Return/g,'&#9166;')
-                                .replace(/Option/g,'&#8997;')
-                                .replace(/Control/g,'&#94;')
-                                .replace(/ESC/g,'&#9099;')
-                                .replace(/Left/g,'&#8592;')
-                                .replace(/Down/g,'&#8595;')
-                                .replace(/Up/g,'&#8593;')
-                                .replace(/Right/g,'&#8594;')
-                                .replace(/Shift/g,'&#8679;')
-                                .replace(/Eject/g,'&#9167;')
-                                .replace(/CapsLock/g,'&#8682;'));
+      $('.box').html(inputRead.replace(/Command/gi,'&#8984;')
+                                .replace(/Tab/gi,'&#8677;')
+                                .replace(/Return/gi,'&#9166;')
+                                .replace(/Option/gi,'&#8997;')
+                                .replace(/Control/gi,'&#94;')
+                                .replace(/ESC/gi,'&#9099;')
+                                .replace(/Left/gi,'&#8592;')
+                                .replace(/Down/gi,'&#8595;')
+                                .replace(/Up/gi,'&#8593;')
+                                .replace(/Right/gi,'&#8594;')
+                                .replace(/Shift/gi,'&#8679;')
+                                .replace(/Eject/gi,'&#9167;')
+                                .replace(/CapsLock/gi,'&#8682;'));
     });
 
 
